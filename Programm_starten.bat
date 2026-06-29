88aec0205051bdf96755e7a30ad5eda2bcc7fba5
Robby Tonn Services <r.tonn@t-online.de>
2026-06-29 06:53:23 +0200

    Für einfacheres Starten
    Mit dieser Zusatzdatei können Sie unser Programm einfacher starten. Tragen Sie einfach den genauen Pfad ein, wo Sie unser Programm auf Ihrem Computer entpackt haben und schon reicht künftig ein Doppelklick, um es auszuführen.

---
+++ Programm_starten.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Programm wird gestartet. Einen Augenblick bitte...
+
+:: Tragen Sie nachfolgend den kompletten Pfad ein, also wo Sie final die ZIP-Datei entpackt haben. Anschließend können Sie das Programm einfach per Doppelklick starten und müssen nicht mehr den Weg übers Terminal gehen.
+
+cd /d "C:\Users\Captain Robby Tonn\Software\Umlaut-Korrektor\Umlaut-Korrektor"
+python umlaut_korrektor.py
+exit
